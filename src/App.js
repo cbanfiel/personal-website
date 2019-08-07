5f21a8d6aaf8e415c0ad599a0a20316ae6d69bd9
Chad Banfield <cbanfiel@emich.edu>
2019-08-06 20:32:28 -0400

    initalize

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './GENERIC.png';
 import appstore from './appstore.svg';
 import './App.css';
+import ScrollableAnchor from 'react-scrollable-anchor';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           On Paper Sports
         </p>
       </header>
+      <ScrollableAnchor id={'games'}>
       <div className="games">
         <div className="basketball">
           <h2>On Paper Sports Basketball '20</h2>
@@ -20,8 +22,8 @@ function App() {
           <iframe className="iframe" src="https://www.youtube.com/embed/AySQYTJunPI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
           </div>
           <div className="links">
-          <a href='https://play.google.com/store/apps/details?id=com.chadbanfield.onpapersportsbasketball20&hl=en_US&pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img class="storeimg" alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/images/generic/en_badge_web_generic.png'/></a>
-          <a href='https://play.google.com/store/apps/details?id=com.chadbanfield.onpapersportsbasketball20&hl=en_US&pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img class="storeimg" alt='Get it on Google Play' src={appstore}/></a>
+          <a href='https://play.google.com/store/apps/details?id=com.chadbanfield.onpapersportsbasketball20&hl=en_US&pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img class="googlestoreimg" alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/images/generic/en_badge_web_generic.png'/></a>
+          <a href='https://apps.apple.com/us/app/on-paper-sports-basketball-20/id1462680635?mt=8'><img class="storeimg" alt='Get it on The App Store' src={appstore}/></a>
           </div>
         </div>
         <div className ="hockey">
@@ -29,10 +31,13 @@ function App() {
           <p>Coming Soon!</p>
         </div>
         </div>
-      <div className="privacy-policy">
+        </ScrollableAnchor>
+        <ScrollableAnchor id={'privacypolicy'}>
+      <div className="privacy-policy" id="privacy-policy">
       <h1>Privacy Policy</h1>
       <p>This is the privacy policy for all On Paper Sports applications, none of your personal data is stored or collected in any way! So go have fun worry free!</p>
         </div>
+        </ScrollableAnchor>
 
     </div>
   );
